ef382ded7048ce2484ea7fff33add200cf08c1f0
mcdavid95 <mcdavidemereuwa95@gmail.com>
2018-10-23 22:09:34 +0100

    ch(cors): fix cors error

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token');
   next();
 });
 
